acf360b91ecf83614ff45fec7f971ab887bb6110
Alexey Yushin <ayushin@apexlabs.ai>
2020-06-21 22:12:15 +0200

    proper total pages calculation

--- packages/list_bloc/lib/src/page.dart
+++ packages/list_bloc/lib/src/page.dart
@@ -7,10 +7,13 @@ class ListPage<T> {
   final int count;
 
   int get offset => number * size;
-  int get pages => count ~/ size;
+  int get pages => count ~/ size + (count % size == 0 ? 0 : 1);
 
   ListPage<T> withData({List<T> data, int count})
     => ListPage<T>(data: data, number: number, size: size, count: count);
 
   ListPage({this.data = const [], this.number = 0, this.size = kPageSize, this.count = 0});
+
+  @override
+  String toString() => 'page $number size $size of $pages with $runtimeType: ${data?.length} elements $count total';
 }
