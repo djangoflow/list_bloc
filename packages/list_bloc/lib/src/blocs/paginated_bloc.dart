adb2596cc6c6c66343533d7ee10cfd5fc6b483dc
Alexey Yushin <ayushin@apexlabs.ai>
2020-07-20 21:51:27 +0200

    added assert for a tricky debug spot

--- packages/list_bloc/lib/src/blocs/paginated_bloc.dart
+++ packages/list_bloc/lib/src/blocs/paginated_bloc.dart
@@ -1,6 +1,6 @@
+import '../page.dart';
 import '../repositories/paginated_repository.dart';
 import 'data_bloc.dart';
-import '../page.dart';
 
 class PaginatedBloc<T,F> extends DataBloc<ListPage<T>, F> {
   @override
@@ -8,6 +8,12 @@ class PaginatedBloc<T,F> extends DataBloc<ListPage<T>, F> {
 
   @override
   final PaginatedRepository<T,F> repository;
+
+  @override
+  void load([F filter]) {
+    assert(filter != null || state.filter != null);
+    super.load(filter);
+  }
 
   PaginatedBloc(this.repository) : super(repository);
 }
