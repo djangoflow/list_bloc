1e1d3283a9942e7515b6f64d9b2c923af5d75216
Alexis Yushin <ayushin@apexlabs.ai>
2022-03-21 18:30:58 +0700

    listviewbuilder

---
+++ packages/flutter_list_bloc/lib/src/listview_bloc_builder.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart' hide Page;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:list_bloc/list_bloc.dart';
+
+class ListViewBlocBuilder<T, F> extends StatelessWidget {
+  final PaginatedCubit<T, F>? cubit;
+
+  final Widget Function(BuildContext, Data<Page<T>, F> state)? headerBuilder;
+  final Widget Function(BuildContext, Data<Page<T>, F> state)? footerBuilder;
+  final Widget Function(BuildContext, Data<Page<T>, F> state) loadingBuilder;
+  final Widget Function(BuildContext, Data<Page<T>, F> state, int index, T item)
+      itemBuilder;
+  final Widget Function(BuildContext, Data<Page<T>, F> state) emptyBuilder;
+
+  ListViewBlocBuilder(
+      {this.cubit,
+      this.headerBuilder,
+      this.footerBuilder,
+      required this.itemBuilder,
+      required this.loadingBuilder,
+      required this.emptyBuilder});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PaginatedCubit<T, F>, Data<Page<T>, F>>(
+      bloc: cubit,
+      builder: (context, state) => Column(
+        children: [
+          if (headerBuilder != null) headerBuilder!.call(context, state),
+          if (state is Loading && (state.data?.data?.isEmpty ?? true))
+            loadingBuilder(context, state)
+          else if (state.data is Empty)
+            Expanded(child: emptyBuilder(context, state))
+          else
+            Expanded(
+              child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) => itemBuilder(
+                      context, state, index, state.data!.data![index]!),
+                  itemCount: state.data?.count),
+            ),
+          if (footerBuilder != null) footerBuilder!.call(context, state),
+        ],
+      ),
+    );
+  }
+}
