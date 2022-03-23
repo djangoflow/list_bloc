81ccbb66c0104b314b6d155e30f370a895952bd6
Alexis Yushin <ayushin@apexlabs.ai>
2022-03-23 15:33:39 +0700

    guarantee Empty state on empty data

--- packages/list_bloc/lib/src/cubits/data_cubit.dart
+++ packages/list_bloc/lib/src/cubits/data_cubit.dart
@@ -3,18 +3,25 @@ import 'package:bloc/bloc.dart';
 import '../data.dart';
 
 class DataCubit<T, F> extends Cubit<Data<T, F>> {
-  final Future <T> Function([F? filter]) loader;
+  final Future<T> Function([F? filter]) loader;
 
   DataCubit(this.loader, [Data<T, F>? state]) : super(state ?? Data.empty());
 
   void clear() => emit(Data.empty());
 
   Future<void> load([F? filter]) async {
-    emit(Data.loading(data: state.data, filter: state.filter));
+    final f = filter ?? state.filter;
+
+    emit(Data.loading(data: state.data, filter: f));
+
     try {
-      emit(Data(data: await loader(filter), filter: state.filter));
+      final data = await loader(f);
+      if (data == null || (data is Iterable && data.isEmpty)) {
+        emit(Empty(filter: f));
+      }
+      emit(Data(data: data, filter: f));
     } catch (e) {
-      emit(Data.error(data: state.data, filter: state.filter, error: e));
+      emit(Data.error(data: state.data, filter: f, error: e));
     }
   }
 }
