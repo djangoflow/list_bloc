36829758dd8392720d26343d90d42761d6a90610
chhannan <ch.hannan15@gmail.com>
2023-01-02 07:06:02 +0500

    Added TODO methods for list cubit class

--- packages/list_bloc/lib/src/cubits/list_cubit.dart
+++ packages/list_bloc/lib/src/cubits/list_cubit.dart
@@ -20,5 +20,62 @@ class ListCubit<T, F> extends DataCubit<List<T>, F> {
       rethrow;
     }
   }
+
 // TODO(alexis): implement remove, add, replace etc
+
+  Future<void> remove(int index, [F? filter]) async {
+    final f = filter ?? state.filter;
+
+    emit(Data.loading(data: state.data, filter: f));
+    try {
+      final data = [...state.data ?? []];
+      data.removeAt(index);
+      emit(
+        data.isEmpty
+            ? Data.empty(filter: f)
+            : Data(data: data, filter: state.filter),
+      );
+    } on Exception catch (e) {
+      emit(Data.error(data: state.data, filter: state.filter, error: e));
+      rethrow;
+    }
+  }
+
+  Future<void> insert(int index, [F? filter]) async {
+    final f = filter ?? state.filter;
+
+    emit(Data.loading(data: state.data, filter: f));
+    try {
+      final items = await loader(f);
+      final data = [...state.data ?? []];
+      data.insertAll(index, items);
+      emit(
+        data.isEmpty
+            ? Data.empty(filter: f)
+            : Data(data: data, filter: state.filter),
+      );
+    } on Exception catch (e) {
+      emit(Data.error(data: state.data, filter: state.filter, error: e));
+      rethrow;
+    }
+  }
+
+  Future<void> replace([F? filter]) async {
+    final f = filter ?? state.filter;
+
+    emit(Data.loading(data: state.data, filter: f));
+    try {
+      final data = await loader(f);
+      emit(
+        data.isEmpty
+            ? Data.empty(filter: f)
+            : Data(data: data, filter: state.filter),
+      );
+    } on Exception catch (e) {
+      emit(Data.error(data: state.data, filter: state.filter, error: e));
+      rethrow;
+    }
+  }
+
+
 }
