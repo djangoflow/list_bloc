ec1aa601452e275252bf6c8a171714ea5041522d
adar2378 <saiful@apexive.com>
2024-01-05 19:33:31 +0600

    refactor: move linter ignore in header

--- packages/openapi_repository/lib/openapi_repository.dart
+++ packages/openapi_repository/lib/openapi_repository.dart
@@ -2,7 +2,10 @@ import 'package:build/build.dart';
 import 'src/list_repository_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder generateListRepositoryBuilder(BuilderOptions options) => PartBuilder(
-      [OpenapiRepositoryGenerator()],
-      '.openapi.dart',
-    );
+Builder generateListRepositoryBuilder(BuilderOptions options) =>
+    PartBuilder([OpenapiRepositoryGenerator()], '.openapi.dart',
+        header: '''
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: sort_constructors_first, sort_unnamed_constructors_first
+''');
